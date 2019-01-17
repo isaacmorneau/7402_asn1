b567a644f84de89e346f0bc2ab5dfdfa03704ec2
Isaac Morneau <isaacmorneau@gmail.com>
2019-01-17 18:35:37 +0000

    bit more lean than before

--- frequency_counter.py
+++ frequency_counter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import sys
 from ascii_graph import Pyasciigraph
-fstr = open(sys.argv[1], "r").read()
-print ("\n".join(line for line in Pyasciigraph().graph('letter frequency', [(c,fstr.count(c)) for c in "abcdefghijklmnopqrstuvwxyz"])))
+f=open(sys.argv[1], "r").read()
+print("\n".join(l for l in Pyasciigraph().graph('letter frequency', [(c,f.count(c)) for c in "abcdefghijklmnopqrstuvwxyz"])))
