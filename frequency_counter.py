b514bc9e56c9b2d26284d9859b6d40bc13f85122
Isaac Morneau <isaacmorneau@gmail.com>
2019-01-22 15:34:50 +0000

    fixed formatting

--- frequency_counter.py
+++ frequency_counter.py
@@ -10,6 +10,6 @@ if len(sys.argv) == 3:
     ct = sum(a for i, a in cl)
     sp = [(l,a, a / st * 100) for l,a in sl]
     cp = [(l,a, a / ct * 100) for l,a in cl]
-    print("\n".join(l for l in Pyasciigraph().graph('Task 1:letter frequency', sl)), "Task 2: breaking the cipher", "\n".join("{0} is {1} - match {2:0.2f}%".format(cp[i][0], sp[i][0], 100-abs(cp[i][2] - sp[i][2])) for i in range(len(sp))))
+    print("\n".join(l for l in Pyasciigraph().graph('Task 1:letter frequency', sl)), "\nTask 2: breaking the cipher\n", "\n".join("{0} is {1} - match {2:0.2f}%".format(cp[i][0], sp[i][0], 100-abs(cp[i][2] - sp[i][2])) for i in range(len(sp))))
 else:
     print("usage: {} <source text> <cipher text>".format(sys.argv[0]))
