13384faa4883d4ef5f1783edec882afbf0c30bc5
Isaac Morneau <isaacmorneau@gmail.com>
2019-01-22 15:27:55 +0000

    im being bad and code golfing this

--- frequency_counter.py
+++ frequency_counter.py
@@ -1,27 +1,15 @@
 #!/usr/bin/env python3
 import sys
 from ascii_graph import Pyasciigraph
-
-if len(sys.argv) != 3:
+if len(sys.argv) == 3:
+    s = open(sys.argv[1], "r").read()
+    c = open(sys.argv[2], "r").read()
+    sl = [a for a in sorted([(a,s.count(a)) for a in "abcdefghijklmnopqrstuvwxyz"], key=lambda x: x[1], reverse=True)]
+    cl = [a for a in sorted([(a,c.count(a)) for a in "abcdefghijklmnopqrstuvwxyz"], key=lambda x: x[1], reverse=True)]
+    st = sum(a for i, a in sl)
+    ct = sum(a for i, a in cl)
+    sp = [(l,a, a / st * 100) for l,a in sl]
+    cp = [(l,a, a / ct * 100) for l,a in cl]
+    print("\n".join(l for l in Pyasciigraph().graph('Task 1:letter frequency', sl)), "Task 2: breaking the cipher", "\n".join("{0} is {1} - match {2:0.2f}%".format(cp[i][0], sp[i][0], 100-abs(cp[i][2] - sp[i][2])) for i in range(len(sp))))
+else:
     print("usage: {} <source text> <cipher text>".format(sys.argv[0]))
-    sys.exit(1)
-
-src = open(sys.argv[1], "r").read()
-cnt = open(sys.argv[2], "r").read()
-
-src_lettercounts = [(c,src.count(c)) for c in "abcdefghijklmnopqrstuvwxyz"]
-cnt_lettercounts = [(c,cnt.count(c)) for c in "abcdefghijklmnopqrstuvwxyz"]
-
-print("\n".join(l for l in Pyasciigraph().graph('Task 1:letter frequency', src_lettercounts)))
-
-src_total = sum(c for i, c in src_lettercounts)
-cnt_total = sum(c for i, c in cnt_lettercounts)
-
-src_percents = [(l,c, c / src_total * 100) for l,c in sorted(src_lettercounts, key=lambda x: x[1], reverse=True)]
-cnt_percents = [(l,c, c / cnt_total * 100) for l,c in sorted(cnt_lettercounts, key=lambda x: x[1], reverse=True)]
-
-print("Task 2: breaking the cipher")
-
-for i in range(len(src_percents)):
-    print("{0} likely {1} - match {2:0.2f}%".format(cnt_percents[i][0], src_percents[i][0], 100-abs(cnt_percents[i][2] - src_percents[i][2])))
-
